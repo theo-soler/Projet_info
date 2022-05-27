33e0b2e8a44ae617ffd79c9075882eebfaef3cba
Théo Soler <theo.soler@mines-paristech.fr>
2022-05-27 16:27:05 +0200

    brent

--- petrole_data_europe.py
+++ petrole_data_europe.py
@@ -1,10 +1,10 @@
-import csv
-file  = open('DCOILBRENTEU.csv')
-csvreader = csv.reader(file)
-header = []
-header = next(csvreader)
-rows = []
-for row in csvreader:
-        rows.append(row)
-file.close()
-print(rows)
+# +
+from bs4 import BeautifulSoup
+
+import requests
+# -
+
+import requests
+URL = "https://fred.stlouisfed.org/graph/fredgraph.csv?bgcolor=%23e1e9f0&chart_type=line&drp=0&fo=open%20sans&graph_bgcolor=%23ffffff&height=450&mode=fred&recession_bars=off&txtcolor=%23444444&ts=12&tts=12&width=1168&nt=0&thu=0&trc=0&show_legend=yes&show_axis_titles=yes&show_tooltip=yes&id=DCOILBRENTEU&scale=left&cosd=2017-05-23&coed=2022-05-23&line_color=%234572a7&link_values=false&line_style=solid&mark_type=none&mw=3&lw=2&ost=-99999&oet=99999&mma=0&fml=a&fq=Daily&fam=avg&fgst=lin&fgsnd=2020-02-01&line_index=1&transformation=lin&vintage_date=2022-05-27&revision_date=2022-05-27&nd=1987-05-20"
+response = requests.get(URL)
+open("brent_europe.csv", "wb").write(response.content)
